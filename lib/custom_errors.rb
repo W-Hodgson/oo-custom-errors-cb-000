714a23e84410cabf0f4323b10b62dc077fc030e1
Will Hodgson <will.hodgson@current-rms.com>
2019-08-21 09:50:22 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,7 +9,7 @@ class Person
     self.partner = person
     if person.class != Person
       begin
-        raise PartnerError
+        raise PartnerError => error
       rescue
       end
     else
